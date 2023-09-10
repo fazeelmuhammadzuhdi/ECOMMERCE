e908f5ebafc492b12e643df164a4fea3067b6695
fazeelmuhammadzuhdi <fazeelmuhammadzuhdi@gmail.com>
2023-09-10 09:45:48 +0700

    Memasang Template Frontend

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProductCategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,8 +20,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
